d18209db34144efc8366973473b31276adf4261a
vijaysanjana <anand123@purdue.edu>
2023-04-27 11:12:04 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,50 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from 'next/head'
+import {WiDayLightWind} from 'react-icons/wi';
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai';
+import Image from 'next/image';
+import laptop from 'public/laptop.png'
+import { useState } from 'react';
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+     <div className={darkMode ? 'dark' : ""}>
+      <Head>
+        <title>Sanjana Anand Portfolio</title>
+        <meta name="description" content="" />
+        <link rel="icon" href='/favicon.ico'/>
+      </Head>
+      <main className='bg-white px-10 dark:bg-gray-900'>
+        <section className='min-h-screen'> 
+        <nav className='py-8 mb-12 flex justify-between'>
+          <h1 className='text-2xl font-satoshi dark:text-white'>SA</h1>
+          <ul className='flex items-center space-x-5'>
+            <li>
+              <a href="#" class="inline-block text-black dark:text-white resume-link relative bg-gradient-to-r from-peach to-peach-dark bg-clip-text text-transparent transition-all duration-300 ease-in-out">
+              resumé
+              <span class="absolute bottom-0 left-0 bg-peach-dark h-1 w-0 transition-all duration-300 ease-in-out"></span>
+              </a>
+              </li>
+              <li><WiDayLightWind 
+                    onClick={() => setDarkMode(!darkMode)} 
+                    className={`cursor-pointer text-2xl text-black dark:text-white transition-colors duration-300 'hover:text-peach'} `}
+                    />
+              </li>
+          </ul>
+        </nav>
+        <div className='flex justify-center'>
+          <div className='w-1/2 pl-10 px-20 text-left items-center justify-center p-10'>
+            <h1 className='text-2xl pt-20 dark:text-white'>hi, i'm</h1>
+            <h2 className='text-6xl pb-2 text-lilac-dark font-medium'>sanjana anand</h2>
+            <h3 className='text-3xl pt-3 italic dark:text-white'>cs @ purdue</h3>
+            <h3 className='text-3xl italic dark:text-white'>software engineer</h3>
+          </div>
+          <div className= 'mx-auto w-1/2 items-center drop-shadow-2xl'>
+            <Image src={laptop} alt='laptop'/>
+          </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </section>
+      </main>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        satoshi: "Satoshi",
+      },
+      colors: {
+        'lilac': '#cca9eb',
+        'lilac-light': '#e1c6f4',
+        'lilac-dark': '#b08dd1',
+        'lilac-darker': '#9b6bcc',
+        'lilac-darkest': '#7f38c9',
+        'peach': '#fe948d',
+        'peach-light': '#feb8b2',
+        'peach-dark': '#f77066',
+        'peach-darker': '#f7564a',
+        'peach-darkest': '#f03a35',
+        'sunshine': '#f2de85',
+        'sunshine-light': '#f5e8b8',
+        'sunshine-dark': '#e6d36b',
+        'sunshine-darker': '#d9c938',
+        'sunshine-darkest': '#c9b700',
+        'water': '#8cc9d1',
+        'water-light': '#b3dfe3',
+        'water-dark': '#6abecb',
+        'water-darker': '#4ba9b9',
+        'water-darkest': '#2e8f9e',
       },
     },
   },
   plugins: [],
-}
+};
